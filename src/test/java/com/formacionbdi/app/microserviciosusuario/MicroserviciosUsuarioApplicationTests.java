32d1b3c59b24adc7d52748c1d9d3eefe7b44f318
Manuellink1409 <manuelaft14@gmail.com>
2024-03-07 15:30:02 -0600

    First Commit

---
+++ src/test/java/com/formacionbdi/app/microserviciosusuario/MicroserviciosUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.app.microserviciosusuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
